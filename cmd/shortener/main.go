43186d9dc90e0573b3f2034ba6e6b7a23654f631
Nail Usmanov <nalus_us@mail.ru>
2025-04-09 19:16:11 +0300

    Реализован инкремент 6 (Сокращение URL)

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -6,9 +6,21 @@ import (
 	"github.com/NailUsmanov/practicum-shortener-url/internal/app"
 	"github.com/NailUsmanov/practicum-shortener-url/internal/storage"
 	"github.com/NailUsmanov/practicum-shortener-url/pkg/config"
+	"go.uber.org/zap"
 )
 
 func main() {
+
+	// создаём предустановленный регистратор zap
+	logger, err := zap.NewDevelopment()
+	if err != nil {
+		// вызываем панику, если ошибка
+		panic(err)
+	}
+	defer logger.Sync()
+
+	// делаем регистратор SugaredLogger
+	sugar := logger.Sugar()
 
 	cfg, err := config.NewConfig()
 	if err != nil {
@@ -17,9 +29,9 @@ func main() {
 
 	store := storage.NewMemoryStorage()
 
-	application := app.NewApp(store, cfg.BaseURL)
+	application := app.NewApp(store, cfg.BaseURL, sugar)
 
 	if err := application.Run(cfg.RunAddr); err != nil {
-		panic(err)
+		sugar.Fatalln(err)
 	}
 }
